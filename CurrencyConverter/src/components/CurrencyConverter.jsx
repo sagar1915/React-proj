751c2954689489aacdbf92877b5d71bae5d2207b
sagar <sagar192010@gmail.com>
2023-09-28 06:04:09 +0530

    currency conversion

---
+++ CurrencyConverter/src/components/CurrencyConverter.jsx
@@ -0,0 +1,10 @@
+
+const CurrencyConverter = () => {
+	return (
+		<div className=" text-center p-3">
+			<h1 className="text-3xl text-white">CurrencyConverter</h1>
+		</div>
+	);
+};
+
+export default CurrencyConverter;
